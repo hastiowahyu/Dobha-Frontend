c6f7ef677b6e8a6862370090e4326b49439e2e85
hastiowahyu <hastio.118140187@student.itera.ac.id>
2022-03-16 13:25:43 +0700

    update on detail artikel

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import DetailArtikel from "./Component/Artikel/DetailArtikel";
-import Footer from "./Component/Footer/Footer";
-import Login from "./Component/Register/Login";
-import Register from "./Component/Register/Register";
+import DetailArtikel from "./Component/Page/Artikel/DetailArtikel";
+import Register from "./Component/Page/Register/Register";
+import Footer from './Component/Footer/Footer'
+import Login from './Component/Page/Register/Login'
+import Navbarnya from "./Component/Header/Navbar";
 
 function App() {
   return (
     <div className='App'>
+      <Navbarnya />
       {/* <Routes>
         <Route exact path='/' element={<Register />} />
         <Route path='/login' element={<Login/>} />
         <Route path="/back" element={<Register/>}/>
       </Routes>
       <Footer/> */}
-      <DetailArtikel />
+    <DetailArtikel></DetailArtikel>
     </div>
   );
 }
